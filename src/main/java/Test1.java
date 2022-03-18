f28ffb25eec2cac107d668e1f21eb5d56c704ea8
DESKTOP-SPTNRI8\xm <2234076313@qq.com>
2022-03-18 13:57:51 +0800

    test1 方法更改

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -1,9 +1,10 @@
 public class Test1 {
     public static void main(String[] args) {
-
         System.out.println("主线路 --> test1");
-
         System.out.println("test1 支路方法");
+
+    }
+    public void method1(){
 
     }
 }
