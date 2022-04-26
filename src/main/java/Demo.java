137453cc6e110be1c0edc4d24eb2d2173701d29c
fankai-cyber <66164959+fankai-cyber@users.noreply.github.com>
2022-04-26 17:21:17 +0800

    hah

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Demo {
-    public void test() {
+    public static void main(String[] args) {
         List<String> list = Arrays.asList("aaaa", "bbbb", "cccc");
 
         //对象实例语法	instanceRef::methodName
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,24 @@
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("支路线程 --> 支路线程");
-    }
-    public void testMethod1(){
+import java.util.HashMap;
 
+class Main {
+    public static void main(String[] args) {
+        // 创建一个 HashMap
+        HashMap<Integer, String> sites = new HashMap<>();
+
+        // 往 HashMap 添加一些元素
+        sites.put(1, "Google");
+        sites.put(2, "Runoob");
+        sites.put(3, "Taobao");
+        System.out.println("sites HashMap: " + sites);
+
+
+        // HashMap 不存在该key
+        String weibo = sites.putIfAbsent(4, "Weibo");
+        System.out.println(weibo);
+        System.out.println("change1");
+        // HashMap 中存在 Key
+        String wiki = sites.putIfAbsent(2, "Wiki");
+        System.out.println(wiki);
+        System.out.println("Updated Languages: " + sites);
     }
 }
